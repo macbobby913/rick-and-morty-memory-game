92556c9cdec144d904042c2ecc1d2f8b6082b7af
Mac Tsai <mac.tsai@twbeex.com>
2024-01-24 11:00:19 +0800

    fix : reverse glare position relative to the cursor position

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -8,6 +8,55 @@ type CardProps = {
   alt: string;
 };
 
+const cardRotation = (
+  x: number,
+  y: number,
+  cardHeightHalf: number,
+  cardWidthHalf: number,
+  cardElement: HTMLDivElement
+) => {
+  // define the amount to rotate in the x axis, ex : 15deg ~ -15deg
+  const rotAmountX = 30;
+  // for converting percentage into degrees
+  const rotRatioX = 100 / rotAmountX;
+  // get the percentage of the value of "y" and the value of "cardHeightHalf"
+  const percentageY = (y * 100) / cardHeightHalf;
+  // convert the percentage into degrees
+  const subtractDegX = percentageY / rotRatioX;
+  // get the amount of rotation in degrees
+  // "-1" is for reversing the rotation angle relative to the cursor position
+  const rotX = (rotAmountX - subtractDegX) * -1;
+
+  // define the amount to rotate in the y axis, ex : 15deg ~ -15deg
+  const rotAmountY = 30;
+  const rotRatioY = 100 / rotAmountY;
+  const percentageX = (x * 100) / cardWidthHalf;
+  const degY = percentageX / rotRatioY;
+  const rotY = (degY - rotAmountX) * -1;
+  // actual rotation
+  cardElement.style.transform = `rotateX(${rotX}deg) rotateY(${rotY}deg)`;
+};
+
+const setGlarePosition = (
+  x: number,
+  y: number,
+  midX: number,
+  midY: number,
+  glareElement: HTMLDivElement
+) => {
+  // the distance between the middle of the card's x(or y) position and the cursor's x(or y) position
+  const distX = Math.abs(midX - x);
+  const distY = Math.abs(midY - y);
+  // the x & y position that will be applied to the glare's position
+  const finaleX = x < midX ? midX + distX : midX - distX;
+  const finaleY = y < midY ? midY + distY : midY - distY;
+  glareElement.style.background = `radial-gradient(
+    circle at ${finaleX}px ${finaleY}px,
+    rgba(255, 255, 255, 0.5),
+    transparent
+  )`;
+};
+
 function Card({ src, alt }: CardProps) {
   const cardRef = useRef<HTMLDivElement>(null);
   const glareRef = useRef<HTMLDivElement>(null);
@@ -18,33 +67,24 @@ function Card({ src, alt }: CardProps) {
     if (!cardElement || !glareElement) return;
 
     const handleMouseMove = (ev: MouseEvent) => {
+      // the x & y position of the cursor relative to the card
+      const x = ev.offsetX;
+      const y = ev.offsetY;
       const cardHeightHalf = cardElement.clientHeight / 2;
-      // the amount to rotate in the x axis, ex : 15deg ~ -15deg
-      const rotAmountX = 30;
-      const rotRatioX = 100 / rotAmountX;
-      const percentageY = (ev.offsetY * 100) / cardHeightHalf;
-      const subtractDegX = percentageY / rotRatioX;
-      const rotX = (rotAmountX - subtractDegX) * -1;
-
-      const rotAmountY = 30;
-      const rotRatioY = 100 / rotAmountY;
       const cardWidthHalf = cardElement.clientWidth / 2;
-      const percentageX = (ev.offsetX * 100) / cardWidthHalf;
-      const degY = percentageX / rotRatioY;
-      const rotY = (degY - rotAmountX) * -1;
-      cardElement.style.transform = `rotateX(${rotX}deg) rotateY(${rotY}deg)`;
-
-      // set glare position based on cursor position
-      glareElement.style.background = `radial-gradient(
-      circle at ${ev.offsetX}px ${ev.offsetY}px,
-      rgba(255, 255, 255, 0.5),
-      transparent
-    )`;
+      cardRotation(x, y, cardHeightHalf, cardWidthHalf, cardElement);
+      setGlarePosition(x, y, cardWidthHalf, cardHeightHalf, glareElement);
     };
     cardElement.addEventListener("mousemove", handleMouseMove);
 
-    const handleMouseLeave = (ev: MouseEvent) =>
-      (cardElement.style.transform = `rotateX(0deg)`);
+    const handleMouseLeave = (ev: MouseEvent) => {
+      cardElement.style.transform = `rotateX(0deg) rotateY(0deg)`;
+      glareElement.style.background = ` radial-gradient(
+        circle at 10% 10%,
+        rgba(255, 255, 255, 0.5),
+        transparent
+      )`;
+    };
     cardElement.addEventListener("mouseleave", handleMouseLeave);
 
     return () => {
