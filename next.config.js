03824ecb0a796fab7b8ad1859da07b0f970c69ef
Mac Tsai <mac.tsai@twbeex.com>
2024-08-21 10:06:35 +0800

    feat : Setting up Github Action

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/rick-and-morty-memory-game",
+  output: "export", // <=== enables static exports
   reactStrictMode: false,
   images: {
     remotePatterns: [{ protocol: "https", hostname: "rickandmortyapi.com" }],
