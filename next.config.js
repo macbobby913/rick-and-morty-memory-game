d7341c41edccf3d83a34f3230254e78648d6a436
Mac Tsai <mac.tsai@twbeex.com>
2024-08-22 09:14:27 +0800

    Revert "feat : Setting up Github Action"
    This reverts commit 03824ecb0a796fab7b8ad1859da07b0f970c69ef.
    
    Github Page doesn't support SSR, remove relevant settings.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // basePath: "/rick-and-morty-memory-game",
-  output: "export", // <=== enables static exports
   reactStrictMode: false,
   images: {
     remotePatterns: [{ protocol: "https", hostname: "rickandmortyapi.com" }],
